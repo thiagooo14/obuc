227b2802413ab5c9ee110d3a436bbbcee0137e1f
thiagooo14 <thiagooo15@gmail.com>
2021-07-01 04:14:55 -0300

    mais um pouco

--- JS/index.js
+++ JS/index.js
@@ -11,34 +11,49 @@ const creat = document.getElementById('creat');
 const build = document.getElementById('build');
 const workPlace = document.getElementById('workPlace');
 
-arrLocaisTrabalho.forEach((location) => {
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-  const td2 = document.createElement('td');
-  const td3 = document.createElement('td');
-  const edit = document.createElement('button');
-  const delet = document.createElement('button');
-  const editImg = document.createElement('img');
-  const deletImg = document.createElement('img');
-  td.innerHTML = location.prédio;
-  td2.innerHTML = location.local;
-  tr.className = 'listIten';
-  tr.appendChild(td);
-  tr.appendChild(td2);
+const render = (itn) => {
+  list.innerHTML = '';
+  let index = 0;
+  arrLocaisTrabalho.forEach((location) => {
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const edit = document.createElement('button');
+    const delet = document.createElement('button');
+    const editImg = document.createElement('img');
+    const deletImg = document.createElement('img');
+    td.innerHTML = location.prédio;
+    td2.innerHTML = location.local;
+    tr.className = 'listIten';
+    tr.appendChild(td);
+    tr.appendChild(td2);
+    tr.id = index;
+    
+    editImg.src = '/image/pen.png';
+    editImg.alt = 'image edit';
+    editImg.className = 'btnList';
+    deletImg.src = '/image/trash.png';
+    deletImg.alt = 'image delet';
+    deletImg.className = 'btnList';
+    index += 1;
 
-  editImg.src = '/image/pen.png';
-  editImg.alt = 'image edit';
-  editImg.className = 'btnList';
-  deletImg.src = '/image/trash.png';
-  deletImg.alt = 'image delet';
-  deletImg.className = 'btnList';
+    edit.appendChild(editImg);
+    delet.appendChild(deletImg);
 
-  edit.appendChild(editImg)
-  delet.appendChild(deletImg)
+    td3.appendChild(edit);
+    td3.appendChild(delet);
+    tr.appendChild(td3);
 
-  td3.appendChild(edit)
-  td3.appendChild(delet)
-  tr.appendChild(td3)
+    list.appendChild(tr);
+  });
+};
+const addList = () => {
+  arrLocaisTrabalho.push({ prédio: build.value, local: workPlace.value });
+  console.log(arrLocaisTrabalho);
+  sessionStorage.setItem('arr', JSON.stringify(arrLocaisTrabalho));
+  render();
+};
 
-  list.appendChild(tr);
-});
+creat.addEventListener('click', addList);
+window.onload(render());
